5aabc841461698baa276b6b182e57fcd2e81f312
LucasJ05 <64053529+LucasHeh47@users.noreply.github.com>
2025-07-29 19:40:42 -0700

    Update controller.py

--- controller.py
+++ controller.py
@@ -8,7 +8,7 @@ class Color(Enum):
     BLUE    = (0, 0, 255)
     WHITE   = (255, 255, 255)
     BLACK   = (0, 0, 0)
-    YELLOW  = (255, 255, 50)
+    YELLOW  = (255, 255, 0)
     CYAN    = (0, 255, 255)
     MAGENTA = (255, 0, 255)
     ORANGE  = (255, 80, 0)

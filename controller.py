c0f2de3ebd4d3a72f39c5de206e2f2aa51290c61
LucasJ05 <64053529+LucasHeh47@users.noreply.github.com>
2025-07-29 19:04:40 -0700

    Update controller.py

--- controller.py
+++ controller.py
@@ -9,16 +9,7 @@ data = {
     "colors": ["blue"]
 }
 
-json_str = json.dumps(data)
-http_request = (
-    f"POST / HTTP/1.1\r\n"
-    f"Host: {HOST}\r\n"
-    f"Content-Type: application/json\r\n"
-    f"Content-Length: {len(json_str)}\r\n"
-    f"\r\n"
-    f"{json_str}"
-)
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(http_request.encode())
+    s.sendall(json.dumps(data).encode('utf-8'))
+    s.shutdown(socket.SHUT_WR)  # Tell the server we're done sending
